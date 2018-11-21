68c6019782e82191ab39a15a6a63af66ffa2c724
prioleaud <wilhemnia@yahoo.com>
2018-11-20 22:47:43 -0500

    added submit button and generating random number for each task

--- slider.js
+++ slider.js
@@ -7,8 +7,18 @@ myApp.controller('TestController', function TestController($scope){
         options: {
           floor: 0,
           ceil: 100,
-          step: 10,
+          step: 1,
           showTicks: true
+        }
+    };
+
+    $scope.getRandomNumber = function(){
+        $scope.randomNumber = Math.floor((Math.random()*100)+1);
+      };
+
+    $scope.nextTask = function() {
+        if($scope.slider.value == 70){
+            $scope.slider.minValue = 50;
         }
     };
 });

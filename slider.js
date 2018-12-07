74740058ef1b79ec2a68482bb64f4db35777eb26
prioleaud <wilhemnia@yahoo.com>
2018-12-07 12:00:39 -0500

    committing changes

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 var myApp = angular.module('myapp', ['rzModule','ui.bootstrap']);
-myApp.controller('TestController', function TestController($scope){
+myApp.controller('TestController', function TestController($scope,$window){
 
     $scope.slider = {
         value: 10,
@@ -24,12 +24,16 @@ myApp.controller('TestController', function TestController($scope){
             $scope.num_correct = $scope.num_correct + 1;
             //when done with repition 
             if($scope.num_correct == $scope.fr_value){
+                $window.alert("You may now use your phone")
                 $scope.playTone();
+
+                $scope.num_correct = 0;
                 //I was thinking we could create a pop-up dialog for each session
                 //and then go to main.html after the 3rd session
 
                 
                 if($scope.num_session == 3){
+                    $scope.num_session = 1;
                     window.location.href = "main.html";
                 }
 
